37fb63823eeb0bab0955a399f190010b6ae66b75
Brock Beaudry <brock.beaudry@gmail.com>
2014-09-12 10:42:38 -0700

    Add grunt task to run angular tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-karma');
 
   grunt.initConfig({
     clean: {
@@ -31,10 +32,25 @@ module.exports = function(grunt) {
         src: ['app/js/**/*.js'],
         dest: 'build/bundle.js'
       },
+
+      angulartest: {
+        options: {
+          transform: ['debowerify'],
+          debug: true
+        },
+        src: ['test/angular/**/*test.js'],
+        dest: 'test/angular-testbundle.js'
+      },
+    },
+
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
     },
 
   });
 
+  grunt.registerTask('angulartest', ['browserify:angulartest', 'karma:unit']);
   grunt.registerTask('build:dev', ['clean:dev', 'browserify:dev', 'copy:dev']);
-
 };
