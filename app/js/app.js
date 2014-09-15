f34f5253d8e79e5d64a6219f51d7ff356e7fac05
Brock Beaudry <brock.beaudry@gmail.com>
2014-09-15 11:37:42 -0700

    Refactor calculatorController into separate view

--- app/js/app.js
+++ app/js/app.js
@@ -5,5 +5,19 @@ require('angular-route');
 
 var calculatorApp = angular.module('calculatorApp', ['ngRoute']);
 
+// Controllers
 require('./calculator/calc-controller')(calculatorApp);
+
+// Services
 require('./calculator/services/math-stats')(calculatorApp);
+
+calculatorApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/calculator', {
+      templateUrl: 'views/calculator.html',
+      controller: 'calculatorController'
+    })
+    .otherwise({
+      redirectTo: '/calculator'
+    });
+}]);
